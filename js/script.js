9dfd68cace29eb939fe3c73339df1bd11b7f4dd9
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-05 17:28:10 +0100

    fix : add right discount

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 //chiedere a utente età e km da percorrere
 const age = parseInt(prompt('Inserire la propria età'));
-console.log(age);
 const travelDistance = parseInt(prompt('Inserire quanti km sono da percorrere'));
-console.log(travelDistance);
 //moltiplicare kilometri per 0,21
 const price = travelDistance * 0.21
-console.log(price)
 //applicare sconto del 20% per under 18 e 40% per over 65
-let userAge
+let userDiscount = price
 if (age > 65) {
-    const bigDiscount = price * 40 / 100
-    console.log(bigDiscount)
-    const bigDiscountPrice = price - bigDiscount
-    console.log(bigDiscountPrice)
-} else if (age < 18);
-    const smallDiscount = price * 20 / 100
-    console.log(smallDiscount)
-    const smallDiscountPrice = price - smallDiscount
-    console.log(smallDiscountPrice)
+    console.log('Il discount per anziani è ' + price * 0.6 + '$');
+} else if (age < 18) {
+    console.log('il discount per minorenni è' + price * 0.8 + '$')
+} else {
+
+}
 //arrotondare output ai centesimi
+let priceFix = userDiscount.toFixed(2);
+console.log(priceFix)
 //mostrare risultato
+finalPriceMessage = ('Il costo totale è ' + priceFix + ('$'));
+document.getElementById('message').innerHTML = finalPriceMessage
