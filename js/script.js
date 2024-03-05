e3f97d860188810dc57b2a7745e907ad22900f72
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-05 16:07:08 +0100

    feat: add age and normal price

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //chiedere a utente età e km da percorrere
+const age = parseInt(prompt('Inserire la propria età'));
+console.log(age);
+const travelDistance = parseInt(prompt('Inserire quanti km sono da percorrere'));
+console.log(travelDistance);
 //moltiplicare kilometri per 0,21
+const price = travelDistance * 0.21
+console.log(price)
 //applicare sconto del 20% per under 18 e 40% per over 65
 //arrotondare output ai centesimi
 //mostrare risultato
