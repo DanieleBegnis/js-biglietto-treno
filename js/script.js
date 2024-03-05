6df2a4009a20b854f9d5938d98af5951a08ad446
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-05 15:59:17 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//chiedere a utente età e km da percorrere
+//moltiplicare kilometri per 0,21
+//applicare sconto del 20% per under 18 e 40% per over 65
+//arrotondare output ai centesimi
+//mostrare risultato
